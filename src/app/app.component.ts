746841f1be92e74d6da9fa2a8cdc1585477e53a1
manuela r. neufeld <m.rodriguez@nevintec.com>
2024-09-03 11:08:46 +0200

    commmit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MyMapComponent } from "./my-map/my-map.component";
+import { MapComponent } from '@maplibre/ngx-maplibre-gl';
+import { Map, NavigationControl } from 'maplibre-gl';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MyMapComponent, MapComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-map-libre';
+  title = 'My-map-app';
 }
